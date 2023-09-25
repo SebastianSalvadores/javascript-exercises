7c58e7ab5761b880c07963d61fd61b0ec03f7259
Sebastian Salvadores <sebasalvadores@gmail.com>
2023-09-25 11:19:47 -0300

    exercises 1 through 4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let newString = "";
+    if(number >= 0){
+        for(let i = 1; i <= number; i++){
+            newString += string;
+        }
+        return newString;
+    }else{
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    
+    for(const argument of args){
+        for(let i = 0; i < array.length; i++){
+            if(array[i] === argument){
+                array.splice(i, 1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
