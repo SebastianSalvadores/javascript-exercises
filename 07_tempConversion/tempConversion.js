943207cd2ca402de3ccdbeeec12ba49605f9b925
Sebastian Salvadores <sebasalvadores@gmail.com>
2023-09-25 16:09:58 -0300

    exercises 5 through 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  let celsius = Math.round((farenheit - 32) * (5/9) * 10) / 10;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let farenheit = Math.round((celsius * 9 / 5 + 32) * 10) / 10;
+  return farenheit;
 };
 
 // Do not edit below this line
